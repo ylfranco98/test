34f64baa0df8bde6b02031a7b77f311f6e0d7b84
Yoana <ylfranco98@gmail.com>
2025-03-23 18:30:36 -0400

    first commit

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,6 +1,6 @@
 import "server-only";
 
 import { defineLive } from "next-sanity";
-import { client } from "@sanity/lib/client";
+import { client } from "./client";
 
 export const { sanityFetch, SanityLive } = defineLive({ client });
